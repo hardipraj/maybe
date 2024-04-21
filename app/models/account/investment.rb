a78580aa4bdde93f5b6fcfffb330d41c45b1289c
Hardip Raj <rajhardip96@gmail.com>
2024-04-21 11:18:00 +0530

    Updated Currency and invenstment subtypes

--- app/models/account/investment.rb
+++ app/models/account/investment.rb
@@ -2,15 +2,14 @@ class Account::Investment < ApplicationRecord
   include Accountable
 
   SUBTYPES = [
-    [ "Brokerage", "brokerage" ],
     [ "Pension", "pension" ],
     [ "Retirement", "retirement" ],
-    [ "401(k)", "401k" ],
-    [ "529 plan", "529_plan" ],
     [ "Health Savings Account", "hsa" ],
     [ "Mutual Fund", "mutual_fund" ],
-    [ "Roth IRA", "roth_ira" ],
-    [ "Roth 401k", "roth_401k" ],
-    [ "Angel", "angel" ]
+    [ "Stocks", "stocks" ],
+    [ "Bonds", "bonds" ],
+    [ "EPF", "epf" ],
+    [ "PPF", "ppf" ],
+    [ "NPS", "nps" ]
   ].freeze
 end
